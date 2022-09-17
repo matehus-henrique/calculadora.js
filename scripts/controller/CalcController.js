42a105042f7655460bbe44db6f8268326dedfcec
matehus-henrique <mtheusriki@gmail.com>
2022-09-16 23:47:01 -0300

    MOD

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,22 +1,47 @@
-class CalcController{
+class CalcController {
 
     constructor(){
+
         this._displayCalc = "0";
-        this._dataAtual;
+        this._currentDate;
+        this.initialize();
+
     }
+
+    initialize(){
+
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "15/04/2021";
+        timeEl.innerHTML = "11:14";
+
+    }
+
     get displayCalc(){
+
         return this._displayCalc;
+
     }
 
     set displayCalc(valor){
-        this.displayCalc = valor;
+
+        this._displayCalc = valor;
+
     }
 
-    get dataAtual(){
-        return this._dataAtual;
+    get currentDate(){
+
+        return this._currentDate;
+
     }
 
-    set dataAtual(valor){
-        this._dataAtual= valor;
+    set currentDate(valor){
+
+        this._currentDate = valor;
+
     }
+
 }
